e4cd96ea4f27875a7b7e6e4e381fe91284c19c12
Danilo Sartorelli Barbato <Dadlo@users.noreply.github.com>
2016-07-26 16:05:22 -0300

    Commit inicial

---
+++ README.txt
@@ -0,0 +1,11 @@
+****************************
+Portal de Educação Superior
+****************************
+
+.. contents:: Table of Contents
+   :depth: 2
+
+Overview
+--------
+
+Configurações para portais de Educação Superior
---
+++ src/metodista/site/educacaosuperior/config.py
@@ -0,0 +1,53 @@
+# -*- coding:utf-8 -*-
+from five import grok
+from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
+from Products.CMFPlone import interfaces as st_interfaces
+
+PROJECTNAME = 'metodista.site.educacaosuperior'
+
+DEPS = [
+    'Products.PloneFormGen',
+    'collective.cover',
+    'collective.polls',
+    'plone.app.theming',
+    'plone.app.contenttypes',
+]
+
+HIDDEN_PROFILES = [
+    'collective.cover:default',
+    'collective.cover:testfixture',
+    'collective.cover:uninstall',
+    'collective.polls:default',
+    'plone.app.blocks:default',
+    'plone.app.dexterity:default',
+    'plone.app.drafts:default',
+    'plone.app.iterate:plone.app.iterate',
+    'plone.app.openid:default',
+    'plone.app.relationfield:default',
+    'plone.app.tiles:default',
+    'plone.formwidget.autocomplete:default',
+    'plone.formwidget.contenttree:default',
+    'Products.PloneFormGen:default',
+]
+
+
+class HiddenProducts(grok.GlobalUtility):
+
+    grok.implements(qi_interfaces.INonInstallable)
+    grok.provides(qi_interfaces.INonInstallable)
+    grok.name(PROJECTNAME)
+
+    def getNonInstallableProducts(self):
+        products = []
+        products = [p for p in DEPS]
+        return products
+
+
+class HiddenProfiles(grok.GlobalUtility):
+
+    grok.implements(st_interfaces.INonInstallable)
+    grok.provides(st_interfaces.INonInstallable)
+    grok.name(PROJECTNAME)
+
+    def getNonInstallableProfiles(self):
+        return HIDDEN_PROFILES
